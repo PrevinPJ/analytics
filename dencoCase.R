64c04ace92b17fad1670c002ade0b038ac94b3cb
PrevinPJ <previn.p.j@gmail.com>
2018-10-01 19:38:25 +0530

    day2

--- dencoCase.R
+++ dencoCase.R
@@ -14,7 +14,7 @@ head(sales1)
 str(sales2)
 
 #read file: Method3
-install.packages('gsheet')
+#install.packages('gsheet')
 library(gsheet)
 url = "https://docs.google.com/spreadsheets/d/1h7HU0X_Q4T5h5D1Q36qoK40Tplz94x_HZYHOJJC_edU/edit#gid=216113907"
 sales3 = as.data.frame(gsheet2tbl(url))
